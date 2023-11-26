eeaa0dedbc6b49dd07b6cfbd0e68605538cb5776
barisAkgul <barisakgul1994@gmail.com>
2023-11-27 02:47:20 +0300

    Added post request for shopping cart

--- src/helpers/api/requests.ts
+++ src/helpers/api/requests.ts
@@ -1,5 +1,14 @@
-import { ISessionIDProps, ProductListResponse } from "~/types/general";
+import axios from "axios";
+
+import {
+  ISessionIDProps,
+  IShoppingCartItemProps,
+  ProductListResponse,
+  ShoppingCartResponse,
+} from "~/types/general";
 import { _getApiRequest } from "./request.helper";
+
+// GET REQUESTS
 
 export const getProductsList = (): Promise<ProductListResponse> =>
   _getApiRequest({
@@ -12,3 +21,63 @@ export const getSessionID = (): Promise<ISessionIDProps> =>
     requestEndpoint:
       "https://linkedin-cv-crawler.beta-limited.workers.dev/interview/createsession",
   });
+
+export const getShoppingCartList = (
+  sessionId: string | undefined
+): Promise<ShoppingCartResponse> =>
+  _getApiRequest({
+    requestEndpoint:
+      "https://linkedin-cv-crawler.beta-limited.workers.dev/interview/view-cart",
+    sessionId,
+  });
+
+export const getSearchProductListWithName = (
+  productName: string
+): Promise<IShoppingCartItemProps> =>
+  _getApiRequest({
+    requestEndpoint: `https://linkedin-cv-crawler.beta-limited.workers.dev/interview/search?name=${productName}`,
+  });
+
+//POST REQUESTS
+
+export const addToCart = async (
+  productId: string,
+  sessionId: string | null
+) => {
+  try {
+    const addToCartEndpoint = `https://linkedin-cv-crawler.beta-limited.workers.dev/interview/add-to-cart?id=${productId}`;
+
+    await axios.post(
+      addToCartEndpoint,
+      {},
+      {
+        headers: {
+          "Session-ID": sessionId,
+        },
+      }
+    );
+  } catch (error: any) {
+    console.error("Error adding to cart:", error.message);
+  }
+};
+
+export const subtractFromCart = async (
+  productId: string,
+  sessionId: string | null
+) => {
+  try {
+    const subtractFromCartEndpoint = `https://linkedin-cv-crawler.beta-limited.workers.dev/interview/subtract-from-cart?id=${productId}`;
+
+    await axios.post(
+      subtractFromCartEndpoint,
+      {},
+      {
+        headers: {
+          "Session-ID": sessionId,
+        },
+      }
+    );
+  } catch (error: any) {
+    console.error("Error subtracting from cart:", error.message);
+  }
+};
