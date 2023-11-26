b12c8b54b46ea2f4db0e9e63c7240a6d982f6ab7
barisAkgul <barisakgul1994@gmail.com>
2023-11-26 18:00:23 +0300

    Material ui theme was added to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import MainLayouts from "./layouts/MainLayouts";
+import { ThemeProvider } from "@mui/material/styles";
+
+import { theme } from "./configs/theme";
+
+import "./styles/reset.css";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <MainLayouts />
-    </>
+    </ThemeProvider>
   );
 }
 
