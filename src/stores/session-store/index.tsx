389d2a4ab6118e0e9da48e5bdf2e1cc4a2dd8f9c
barisAkgul <barisakgul1994@gmail.com>
2023-11-27 04:57:29 +0300

    Unnecessary lines deleted

--- src/stores/session-store/index.tsx
+++ src/stores/session-store/index.tsx
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import { getSessionID } from "~/helpers/api/requests";
-import { ISessionIDProps } from "~/types/general";
 
 interface SessionStore {
   sessionId: string | null;
